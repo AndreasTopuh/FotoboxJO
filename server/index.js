cf9cee8746649460c23572ea7ec7bc2b30c31b86
davidkndou <s22110199@student.unklab.ac.id>
2025-07-19 17:51:24 +0800

    changes server.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://gofotobox.online'],
+  origin: ['http://localhost:5173', 'https://gofotobox.online/'],
   methods: ['GET', 'POST'],
   credentials: true,
 }));
