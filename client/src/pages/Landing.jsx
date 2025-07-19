20852b42ee249c58eb853148f4464430f18ed7ae
davidkndou <s22110199@student.unklab.ac.id>
2025-07-19 11:25:20 +0800

    change ui landing

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,30 +1,19 @@
 import { useNavigate } from 'react-router-dom';
-import bgImg from '../assets/grid-bg.png';
 
 export default function Landing() {
   const navigate = useNavigate();
-
   return (
-    <div
-      className="relative w-screen h-screen overflow-hidden text-white"
-      style={{
-        backgroundImage: `url(${bgImg})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
+    <div className="relative w-screen h-screen overflow-hidden text-white">
       {/* Container Tengah */}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
         {/* Ilustrasi dan Teks */}
-        <div className="relative mb-10">
-          <div className="text-5xl font-extrabold drop-shadow-lg">FotoboxJO</div>
+        <div className="text-4xl lg:text-5xl xl:text-6xl font-extrabold drop-shadow-lg">
+          FotoboxJO
         </div>
-
         {/* Tombol */}
         <button
-          className="bg-pink-400 hover:bg-pink-500 transition px-8 py-3 rounded-full text-lg font-semibold shadow-md text-white"
-          onClick={() => navigate('/payment')}
-        >
+          className="bg-pink-400 hover:bg-pink-500 transition px-6 py-3 lg:px-8 lg:py-4 xl:px-10 xl:py-5 rounded-full text-base lg:text-lg xl:text-xl font-semibold shadow-md text-white"
+          onClick={() => navigate('/payment')}>
           Ketuk Untuk Mulai
         </button>
       </div>
