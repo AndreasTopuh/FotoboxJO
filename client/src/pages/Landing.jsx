8e976d1f64c59365d1530291ae29c7a5a1c13446
davidkndou <s22110199@student.unklab.ac.id>
2025-07-21 12:10:59 +0800

    change layout landing

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -4,7 +4,7 @@ export default function Landing() {
   const navigate = useNavigate();
 
   return (
-    <div className="relative w-screen h-screen overflow-hidden text-white bg-gray-900">
+    <div className="relative w-screen h-screen overflow-hidden text-white">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div className="w-[90vw] max-w-3xl backdrop-blur-md bg-white/10 border border-white/20 rounded-2xl px-10 py-16 text-center shadow-2xl">
           <div className="text-5xl lg:text-6xl font-extrabold drop-shadow-lg mb-10">
